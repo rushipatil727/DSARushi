01b598523efb6034fab1bf4112fba954aec29ddc
Rushikesh1 Patil <rushikesh1.patil@lntinfotech.com>
2023-10-27 11:56:54 +0530

    Hi

--- DSA/src/com/linkedlist/Lis2.java
+++ DSA/src/com/linkedlist/Lis2.java
@@ -4,6 +4,6 @@ public class Lis2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("LTIMindtree is the bigg ");
+		System.out.println("LTIMindtree is the bigg company");
 	}
 }
